585470d1a1f07b5e3dd19ba516e0e24c2cce73bf
morita657 <morita657@gmail.com>
2018-08-17 22:41:43 +0900

    Make LInked Lisr Cycle

--- Linked List Cycle.js
+++ Linked List Cycle.js
@@ -11,13 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(head === null) return false;
-    let fast = head.next;
-    let slow = head;
-    while(fast && fast.next && slow){
-        if(fast.val === slow.val) return true;
-        fast = fast.next.next;
-        slow = slow.next;
-    }
-    return false;
+  if (head === null) return false;
+  let fast = head.next;
+  let slow = head;
+  while (fast && fast.next && slow) {
+    if (fast.val === slow.val) return true;
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return false;
 };
