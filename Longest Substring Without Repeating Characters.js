c077dec1f96e5e6c44ad2155ac8167160d9c5fa1
morita657 <morita657@gmail.com>
2018-09-01 07:45:24 +0900

    Refactor using map

--- Longest Substring Without Repeating Characters.js
+++ Longest Substring Without Repeating Characters.js
@@ -4,18 +4,14 @@
  */
 var lengthOfLongestSubstring = function(s) {
   const n = s.length;
-  const set = new Set();
-  let ans = 0,
-    i = 0,
-    j = 0;
-  while (i < n && j < n) {
-    // try to extend the range [i,j]
-    if (!set.has(s.charAt(j))) {
-      set.add(s.charAt(j++));
-      ans = Math.max(ans, j - i);
-    } else {
-      set.delete(s.charAt(i++));
+  let ans = 0;
+  const map = new Map();
+  for (let j = 0, i = 0; j < n; j++) {
+    if (map.has(s.charAt(j))) {
+      i = Math.max(map.get(s.charAt(j)), i);
     }
+    ans = Math.max(ans, j - i + 1);
+    map.set(s.charAt(j), j + 1);
   }
   return ans;
 };
