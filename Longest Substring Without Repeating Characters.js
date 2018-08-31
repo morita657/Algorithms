5067ed3af0d81f38b482d36edb011f1f371fbfba
morita657 <morita657@gmail.com>
2018-08-31 23:40:36 +0900

    Refactor using Sliding Window

--- Longest Substring Without Repeating Characters.js
+++ Longest Substring Without Repeating Characters.js
@@ -4,20 +4,18 @@
  */
 var lengthOfLongestSubstring = function(s) {
   const n = s.length;
-  let ans = 0;
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j <= n; j++) {
-      if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+  const set = new Set();
+  let ans = 0,
+    i = 0,
+    j = 0;
+  while (i < n && j < n) {
+    // try to extend the range [i,j]
+    if (!set.has(s.charAt(j))) {
+      set.add(s.charAt(j++));
+      ans = Math.max(ans, j - i);
+    } else {
+      set.delete(s.charAt(i++));
     }
   }
   return ans;
-};
-const allUnique = (s, start, end) => {
-  const set = new Set();
-  for (let i = start; i < end; i++) {
-    let ch = s.charAt(i);
-    if (set.has(ch)) return false;
-    set.add(ch);
-  }
-  return true;
 };
