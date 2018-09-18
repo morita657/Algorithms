58e8a654cfde20ab67fb2e1c5abb5870dca55fce
morita657 <morita657@gmail.com>
2018-09-18 14:44:12 +0900

    Make String to Integer (atoi)

---
+++ String to Integer (atoi) .js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+  if (isNaN(parseInt(str))) return 0;
+  if (parseInt(str) < -2147483648) return -2147483648;
+  else if (parseInt(str) > 2147483647) return 2147483647;
+  else {
+    return parseInt(str);
+  }
+};
