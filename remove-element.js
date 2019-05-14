39c4cc4265517f1bd401b3ebbe55fef9717beba4
morita657 <morita657@gmail.com>
2019-05-14 22:45:18 +0900

    Second try

--- remove-element.js
+++ remove-element.js
@@ -4,23 +4,15 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] === val && i !== 0) {
-      nums = nums.slice(0, i).concat(nums.slice(i + 1));
-    } else if (nums[i] === val && i == 0) {
-      nums = nums.slice(1);
+  let front = 0,
+    back = nums.length;
+  while (front < back) {
+    if (nums[front] === val) {
+      nums[front] = nums[back - 1];
+      back--;
+    } else {
+      front++;
     }
-    i++;
   }
-  while (i > 0) {
-    if (nums[i] === val && i !== nums.length - 1) {
-      nums = nums.slice(0, i).concat(nums.slice(i + 1));
-    } else if (nums[i] === val && i == nums.length) {
-      nums = nums.slice(0, i - 1);
-    }
-    i--;
-  }
-
-  return nums;
+  return back;
 };
