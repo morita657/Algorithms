b732039f15ef94888be9245b8af977eaf4d08b0e
Takahiro Morita <morita657@gmail.com>
2019-05-25 22:00:28 +0900

    Add selection sort

---
+++ selection_sort.js
@@ -0,0 +1,15 @@
+function selectionSort(A,N){
+    for(let i =0; i<N; i++){
+        minj = i;
+        for(let j = i; j < N; j++){
+            if(A[j] < A[minj]){
+                minj = j;
+            }
+        }
+        sw = A[i];
+        A[i] = A[minj];
+        A[minj] = sw;
+    }
+    return A;
+}
+
