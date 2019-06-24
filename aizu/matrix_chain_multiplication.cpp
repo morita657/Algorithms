a1200e96bcfb2b5eab4eedd7d212a641430464e5
morita657 <morita657@gmail.com>
2019-06-25 00:14:02 +0900

    Fix bug

--- aizu/matrix_chain_multiplication.cpp
+++ aizu/matrix_chain_multiplication.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int n, p[N + 1], m[N + 1][N + 1];
     cin >> n;
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> p[i - 1] >> p[i];
     }
@@ -17,13 +17,13 @@ int main()
     {
         m[i][i] = 0;
     }
-    for (int i = 2; i <= n; i++)
+    for (int l = 2; l <= n; l++)
     {
-        for (int l = 1; l <= n - l; l++)
+        for (int i = 1; i <= n - l + 1; i++)
         {
             int j = i + l - 1;
             m[i][j] = (1 << 21);
-            for (int k = 1; k <= j - 1; k++)
+            for (int k = i; k <= j - 1; k++)
             {
                 m[i][j] = min(m[i][j], m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]);
             }
