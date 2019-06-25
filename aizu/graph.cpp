cd86606e0082f0bd584c7ff1353a5ac6c630a5bf
morita657 <morita657@gmail.com>
2019-06-25 21:54:25 +0900

    Update graph

--- aizu/graph.cpp
+++ aizu/graph.cpp
@@ -4,9 +4,10 @@ static const int N = 100;
 
 int main()
 {
-    int M[N][N];
     int n, u, k, v;
+    int M[N][N];
     cin >> n;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -14,26 +15,22 @@ int main()
             M[i][j] = 0;
         }
     }
+
     for (int i = 0; i < n; i++)
     {
         cin >> u >> k;
-        u--;
         for (int j = 0; j < k; j++)
         {
             cin >> v;
-            v--;
-            M[u][v] = 1;
+            M[u][v - 1] = 1;
         }
     }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (j)
-            {
-                cout << " ";
-            }
-            cout << M[i][j];
+            cout << M[i][j] << " ";
         }
         cout << endl;
     }
