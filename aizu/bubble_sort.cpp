1c1c3e3de9eee4b6c5eefd6a351ba4352dabbe7b
morita657 <morita657@gmail.com>
2019-07-05 01:32:45 +0900

    Update bubble sort

--- aizu/bubble_sort.cpp
+++ aizu/bubble_sort.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
 static const int MAX = 100;
-=======
->>>>>>> 4600b1ff8059f999868990b48e25517fde96d37a
 
 int bubbleSort(int A[], int N)
 {
@@ -28,7 +25,6 @@ int bubbleSort(int A[], int N)
 
 int main()
 {
-<<<<<<< HEAD
     int A[MAX], j, N, sw;
     cin >> N;
     for (int k = 0; k < N; k++)
@@ -36,9 +32,6 @@ int main()
         cin >> j;
         A[k] = j;
     }
-=======
-    int A[10] = {1, 6, 3, 7, 9, 423, 76, 432, 0, 64}, N = 10, sw;
->>>>>>> 4600b1ff8059f999868990b48e25517fde96d37a
     sw = bubbleSort(A, N);
     for (int i = 0; i < N; i++)
     {
