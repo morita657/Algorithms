8165be81d811f3951a23ac7d08b9f60737ef1aec
morita657 <morita657@gmail.com>
2019-07-12 19:26:45 +0900

    Update the performance

--- Leetcode_solutions/pascals-triangle.js
+++ Leetcode_solutions/pascals-triangle.js
@@ -3,27 +3,20 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-  const first = [1],
-    second = [1, 1],
-    result = [first, second];
-  if (numRows === 0) return [];
-  else if (numRows === 1) return [[1]];
-  else if (numRows === 2) return [[1], [1, 1]];
-  else {
-    numRows = numRows - 2;
-    while (numRows !== 0) {
-      elem = result[result.length - 1];
-      newRow = [1];
-      for (let i = 0; i < elem.length - 1; i++) {
-        newElem = elem[i] + elem[i + 1];
-        newRow.push(newElem);
-      }
-      newRow.push(1);
-      result.push(newRow);
-      numRows--;
+  const triangle = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = [];
+    for (let j = 0; j < i + 1; j++) {
+      row.push(0);
     }
-    return result;
+    row[0] = 1;
+    row[row.length - 1] = 1;
+    for (let k = 1; k < row.length - 1; k++) {
+      row[k] = triangle[i - 1][k - 1] + triangle[i - 1][k];
+    }
+    triangle.push(row);
   }
+  return triangle;
 };
 
 console.log(generate(5));
