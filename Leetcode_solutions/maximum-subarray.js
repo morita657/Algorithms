54d02ee30bab0764de1a6bc8b6abfe318ca6e108
morita657 <morita657@gmail.com>
2019-07-16 21:17:50 +0900

    Update approach

--- Leetcode_solutions/maximum-subarray.js
+++ Leetcode_solutions/maximum-subarray.js
@@ -3,26 +3,10 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  max_sum = Number.NEGATIVE_INFINITY;
-  let i = 0,
-    j = 0;
-  while (i < nums.length) {
-    let sum = 0;
-    while (j < nums.length) {
-      sum += nums[j];
-      if (sum > max_sum) {
-        max_sum = sum;
-      }
-      if (nums[j] > sum) {
-        break;
-      } else {
-        j++;
-      }
-    }
-    i = j;
+  (soFar = nums[0]), (maxSum = nums[0]);
+  for (let i = 1; i < nums.length; i++) {
+    soFar = Math.max(nums[i], (soFar += nums[i]));
+    maxSum = Math.max(soFar, maxSum);
   }
-  return max_sum;
+  return maxSum;
 };
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-
-console.log(maxSubArray([-1]));
