e067209f56a5607364e7d19b60f83e2f80102dda
morita657 <morita657@gmail.com>
2019-07-25 21:58:29 +0900

    Add min stack

---
+++ Leetcode_solutions/min-stack.py
@@ -0,0 +1,44 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop(len(self.stack) - 1)
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[len(self.stack) - 1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        min = self.stack[0]
+        for i in self.stack:
+            if i < min:
+                min = i
+        return min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
