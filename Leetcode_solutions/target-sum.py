63aa80b82c104c44e2e6546c6285aa048de9d311
morita657 <morita657@gmail.com>
2019-07-27 14:44:33 +0900

    Add dp style

--- Leetcode_solutions/target-sum.py
+++ Leetcode_solutions/target-sum.py
@@ -1,22 +1,17 @@
 class Solution(object):
-    def __init__(self):
-        self.cnt = 0
-
     def findTargetSumWays(self, nums, S):
         """
         :type nums: List[int]
         :type S: int
         :rtype: int
         """
-        i = 0
-        current = 0
-        self.compute(nums, S, i, current)
-        return self.cnt
-
-    def compute(self, nums, S, index, v):
-        if index == len(nums):
-            if v == S:
-                self.cnt = self.cnt + 1
-        else:
-            self.compute(nums, S, index + 1, v + nums[index])
-            self.compute(nums, S, index + 1, v - nums[index])
+        if not nums:
+            return 0
+        dic = {nums[0]: 1, -nums[0]: 1} if nums[0] != 0 else {0: 2}
+        for i in range(1, len(nums)):
+            tdic = {}
+            for d in dic:
+                tdic[d + nums[i]] = tdic.get(d + nums[i], 0) + dic.get(d, 0)
+                tdic[d - nums[i]] = tdic.get(d - nums[i], 0) + dic.get(d, 0)
+            dic = tdic
+        return dic.get(S, 0)
