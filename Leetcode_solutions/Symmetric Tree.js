e9d11e058562ec5cad2ec3796fdef11218c09c5e
morita657 <morita657@gmail.com>
2019-07-27 11:00:43 +0900

    Add synnetric tree

--- Leetcode_solutions/Symmetric Tree.js
+++ Leetcode_solutions/Symmetric Tree.js
@@ -10,16 +10,38 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-  return mirror(root, root);
-};
-const mirror = (node1, node2) => {
-  if (!node1 && !node2) return true;
-  if ((!node1 && node2) || (node1 && !node2)) return false;
-  if (node1 && node2) {
-    return (
-      node1.val === node2.val &&
-      mirror(node1.left, node2.right) &&
-      mirror(node1.right, node2.left)
-    );
+  if (!root) {
+    // Sanity check
+    return true;
   }
+
+  function isMirror(s, t) {
+    var q1 = [s],
+      q2 = [t];
+
+    // Perform breadth-first search
+    while (q1.length > 0 || q2.length > 0) {
+      // Dequeue
+      var n1 = q1.shift(),
+        n2 = q2.shift();
+
+      // Two null nodes, let's continue
+      if (!n1 && !n2) continue;
+
+      // Return false as long as there is a mismatch
+      if (!n1 || !n2 || n1.val !== n2.val) return false;
+
+      // Scan tree s from left to right
+      // and scan tree t from right to left
+      q1.push(n1.left);
+      q1.push(n1.right);
+      q2.push(n2.right);
+      q2.push(n2.left);
+    }
+
+    return true;
+  }
+  return isMirror(root.left, root.right);
 };
+
+console.log(isSymmetric([1, 2, 2, 3, 4, 4, 3]));
