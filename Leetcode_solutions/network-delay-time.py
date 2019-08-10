def311c2a6ac83771907adebb0482bd511ff0da6
morita657 <morita657@gmail.com>
2019-08-10 22:58:56 +0900

    Add heapq and dijkstra style

--- Leetcode_solutions/network-delay-time.py
+++ Leetcode_solutions/network-delay-time.py
@@ -58,3 +58,29 @@ class Solution(object):
 
 s = Solution().networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2)
 print(s)
+
+import heapq
+
+
+class Solution(object):
+    def networkDelayTime(self, times, N, K):
+        graph = defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        pq = [(0, K)]
+        dist = {}
+        while pq:
+            d, node = heapq.heappop(pq)
+            if node in dist:
+                continue
+            dist[node] = d
+            for nei, d2 in graph[node]:
+                if nei not in dist:
+                    heapq.heappush(pq, (d + d2, nei))
+
+        return max(dist.values()) if len(dist) == N else -1
+
+
+s = Solution().networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2)
+print(s)
