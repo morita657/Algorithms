a240eb0fa8d530425f79bed1ff613d1ac3f00028
morita657 <morita657@gmail.com>
2019-08-16 14:24:58 +0900

    Add is-subsequence

---
+++ Leetcode_solutions/is-subsequence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        for c in s:
+            i = t.find(c)
+            if i == -1:
+                return False
+            else:
+                t = t[i + 1:]
+        return True
