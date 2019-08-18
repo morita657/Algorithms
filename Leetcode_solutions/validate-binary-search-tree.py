7b91c1088c675d752d51ce06b0f54f0867fcf6df
morita657 <morita657@gmail.com>
2019-08-18 19:09:50 +0900

    add another solution

--- Leetcode_solutions/validate-binary-search-tree.py
+++ Leetcode_solutions/validate-binary-search-tree.py
@@ -19,3 +19,26 @@ class Solution(object):
         if not root.val < high:
             return False
         return self.isValidBST(root.left, low, min(root.val, high)) and self.isValidBST(root.right, max(root.val, low), high)
+
+
+class Solution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(root)
