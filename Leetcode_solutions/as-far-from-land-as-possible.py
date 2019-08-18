1077e0ae65a3612d48cb5ff0b464a7c36526cd89
morita657 <morita657@gmail.com>
2019-08-18 14:48:07 +0900

    Add as-far-from-land-as-possible

---
+++ Leetcode_solutions/as-far-from-land-as-possible.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        q = deque([(i, j) for i in range(m)
+                   for j in range(n) if grid[i][j] == 1])
+        if len(q) == m * n or len(q) == 0:
+            return -1
+        level = 0
+        while q:
+            size = len(q)
+            for _ in range(size):
+                i, j = q.popleft()
+                for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    xi, yj = x + i, y + j
+#                     x and y are within the range, next location is 0
+                    if 0 <= xi < m and 0 <= yj < n and grid[xi][yj] == 0:
+                        q.append((xi, yj))
+                        grid[xi][yj] = 1
+                        print(xi, yj, level)
+            level += 1
+        return level - 1
