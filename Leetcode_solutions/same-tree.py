2ee857e9581ea25d90224309b6b204f0d08d86f4
morita657 <morita657@gmail.com>
2019-08-21 23:12:07 +0900

    Add not recursive style

--- Leetcode_solutions/same-tree.py
+++ Leetcode_solutions/same-tree.py
@@ -20,3 +20,41 @@ class Solution(object):
         if p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# Non-recursive style
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        #         if p and q does not exists
+        #         if p and q exists
+        #         if either p or q exists
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+
+        queue = [(p, q)]
+
+        while len(queue) > 0:
+            print(queue)
+            now = queue[0]
+            queue.pop(0)
+            if not check(now[0], now[1]):
+                return False
+            if now[0]:
+                queue.append((now[0].left, now[1].left))
+                queue.append((now[0].right, now[1].right))
+        return True
