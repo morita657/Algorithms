9ef369f8241ed4f692153608e08e9fe52b3b3b83
morita657 <morita657@gmail.com>
2019-08-25 08:47:17 +0900

    add binary tree postorder

---
+++ Leetcode_solutions/binary-tree-postorder-traversal.py
@@ -0,0 +1,29 @@
+binary - tree - postorder - traversal
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+
+        stack, output = [root, ], []
+        while stack:
+            root = stack.pop()
+            output.append(root.val)
+            if root.left is not None:
+                stack.append(root.left)
+            if root.right is not None:
+                stack.append(root.right)
+
+        return output[::-1]
