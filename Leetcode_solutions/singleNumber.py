b00f71a7ea2fbe4b5cf514d5ae4e50f52b56202c
morita657 <morita657@gmail.com>
2019-09-14 18:23:00 +0900

    Add single number

---
+++ Leetcode_solutions/singleNumber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        memory = collections.defaultdict(int)
+        for i in range(len(nums)):
+            if nums[i] in memory.keys():
+                memory[nums[i]] += 1
+            else:
+                memory[nums[i]] = 1
+        ans = 0
+        for index, key in enumerate(memory):
+            if memory[key] == 1:
+                ans = key
+        return ans
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
