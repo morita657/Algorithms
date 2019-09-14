f4822970d88ce7d1e7c29c491373f082ea940f8c
morita657 <morita657@gmail.com>
2019-09-14 19:36:08 +0900

    add two sum

--- Leetcode_solutions/two-sum.py
+++ Leetcode_solutions/two-sum.py
@@ -7,3 +7,25 @@ class Solution:
                     ans = [i, j]
                     break
         return ans
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        q = nums[:]
+        while True:
+            now = q[0]
+            index = nums.index(now)
+            q.pop(0)
+            for i in range(index + 1, len(nums)):
+                if now + nums[i] == target:
+                    return [index, i]
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = collections.defaultdict(list)
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dict.keys():
+                return [dict[complement][0], i]
+            dict[nums[i]].append(i)
