9877b1cfb0c82211b6ca45a6138e3395a57f557f
morita657 <morita657@gmail.com>
2019-09-15 18:08:01 +0900

    Add palindrome linked list

---
+++ Leetcode_solutions/Palindrome_Linked_List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        cand = []
+        while head:
+            cand.append(head.val)
+            head = head.next
+        if len(cand) == 1:
+            return True
+        mid = len(cand) // 2
+        if len(cand) % 2 != 0:
+            return cand[:mid + 1][::-1] == cand[mid:]
+        else:
+            return cand[:mid][::-1] == cand[mid:]
