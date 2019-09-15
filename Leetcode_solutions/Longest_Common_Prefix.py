a7f8582a24128ad11a06dfae75464fc292f02098
morita657 <morita657@gmail.com>
2019-09-15 18:07:50 +0900

    Add longest common prefix

---
+++ Leetcode_solutions/Longest_Common_Prefix.py
@@ -0,0 +1,48 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        current = ""
+        flag = False
+        if len(strs) == 0:
+            return ""
+        elif len(strs) == 1:
+            return strs[0]
+        for i in range(len(strs[0])):
+            current += strs[0][i]
+            for j in range(len(strs)):
+                if strs[j][:i + 1] == current:
+                    continue
+                else:
+                    flag = True
+                    break
+            if flag:
+                break
+        if flag:
+            return current[:-1]
+        return current
+
+# use only differences
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            # iter_target is the shortest one from two elements
+            if len(prefix) < len(strs[i]):
+                iter_target = prefix
+                rest = strs[i]
+            else:
+                iter_target = strs[i]
+                rest = prefix
+            current = ""
+            # take only common part as the prefix
+            for j in range(len(iter_target)):
+                if iter_target[j] == rest[j]:
+                    current += rest[j]
+                else:
+                    prefix = current
+                    break
+            prefix = current
+        return prefix
