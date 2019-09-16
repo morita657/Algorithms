81cdabbfa1ddc0a44d7e43e1eb9e3a118623c801
morita657 <morita657@gmail.com>
2019-09-16 20:41:24 +0900

    Add best time to buy and sell stock

---
+++ Leetcode_solutions/Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minprice = math.inf
+        maxprofit = 0
+        for i in range(len(prices)):
+            if prices[i] < minprice:
+                minprice = prices[i]
+            elif prices[i] - minprice > maxprofit:
+                maxprofit = prices[i] - minprice
+        return maxprofit
