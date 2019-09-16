04d0f4c36091578b3fe067f8601ced5b56b9df8a
morita657 <morita657@gmail.com>
2019-09-16 20:41:42 +0900

    add climbing stairs

---
+++ Leetcode_solutions/Climbing_Stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [[] for i in range(n + 1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1]
+        return dp[n]
