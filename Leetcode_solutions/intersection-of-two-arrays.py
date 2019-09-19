d108f2fce52bb311a2f87d04a941f960671564a7
morita657 <morita657@gmail.com>
2019-09-19 19:27:19 +0900

    add intersection of two arrays

---
+++ Leetcode_solutions/intersection-of-two-arrays.py
@@ -0,0 +1,34 @@
+class Solution:
+    def set_intersection(self, set1, set2):
+        return [x for x in set1 if x in set2]
+
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        if len(set1) < len(set2):
+            return self.set_intersection(set1, set2)
+        else:
+            return self.set_intersection(set2, set1)
+
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        res, set1, visited = [], set(nums1), set()
+        for e in nums2:
+            if e in set1 and e not in visited:
+                res.append(e)
+                visited.add(e)
+            elif e not in set1:
+                visited.add(e)
+        return res
