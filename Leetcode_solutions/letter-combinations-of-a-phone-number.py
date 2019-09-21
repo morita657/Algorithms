a260470a9d588bfa13a2473d8da4d7309854e90f
morita657 <morita657@gmail.com>
2019-09-21 15:12:02 +0900

    add letter combinations of a phone number

--- Leetcode_solutions/letter-combinations-of-a-phone-number.py
+++ Leetcode_solutions/letter-combinations-of-a-phone-number.py
@@ -31,3 +31,23 @@ class Solution:
         if digits:
             backtrack("", digits)
         return output
+
+
+import functools
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if '' == digits:
+            return []
+        kvmaps = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        return functools.reduce(lambda acc, digit: print(acc)[x + y for x in acc for y in kvmaps[digit]], digits, [''])
