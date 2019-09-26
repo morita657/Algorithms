d671b38fef20883498a0557329620df1914a8d13
morita657 <morita657@gmail.com>
2019-09-26 20:18:04 +0900

    add unique paths

--- Leetcode_solutions/Unique Paths.py
+++ Leetcode_solutions/Unique Paths.py
@@ -1,5 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 and n == 1:
+            return 1
+        if m < 1 or n < 1:
+            return 0
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         aux = [[1 for x in range(n)] for x in range(m)]
         for i in range(1, m):
             for j in range(1, n):
