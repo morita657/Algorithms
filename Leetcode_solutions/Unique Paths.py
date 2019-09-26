5e8262dc2388611099ab0125e4f68af867d1b0cd
morita657 <morita657@gmail.com>
2019-09-26 21:20:16 +0900

    add unique paths

--- Leetcode_solutions/Unique Paths.py
+++ Leetcode_solutions/Unique Paths.py
@@ -9,8 +9,26 @@ class Solution:
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        aux = [[1 for x in range(n)] for x in range(m)]
+        dp = [[None] * (n + 1) for i in range(m + 1)]
+
+        def move(movem, moven, dp):
+            if dp[movem][moven] != None:
+                return dp[movem][moven]
+            if movem == 1 and moven == 1:
+                return 1
+            if movem < 1 or moven < 1:
+                return 0
+            dp[movem][moven] = move(movem - 1, moven, dp) + \
+                move(movem, moven - 1, dp)
+            return dp[movem][moven]
+        return move(m, n, dp)
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1 for i in range(n)]
         for i in range(1, m):
             for j in range(1, n):
-                aux[i][j] = aux[i][j - 1] + aux[i - 1][j]
-        return aux[-1][-1]
+                dp[j] = dp[j] + dp[j - 1]
+        print(dp)
+        return dp[n - 1]
