7a123d84631562f6e0d0824feb69eefc44b9728b
morita657 <morita657@gmail.com>
2019-09-29 15:04:49 +0900

    add maximum product subarray

--- Leetcode_solutions/Maximum Product Subarray.py
+++ Leetcode_solutions/Maximum Product Subarray.py
@@ -7,3 +7,21 @@ class Solution:
                 current *= nums[j]
                 output.append(current)
         return max(output)
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        time = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 2:
+                    self.dfs(i, j, grid, time)
+        return time
+
+    def dfs(self, i, j, board, time):
+        if i < 0 or i > len(board) or j < 0 or j > len(board[0]) or board[i][j] != 1:
+            return
+        return self.dfs(i + 1, j, board, time + 1) +\
+            self.dfs(i, j + 1, board, time + 1) +\
+            self.dfs(i - 1, j, board, time + 1) +\
+            self.dfs(i, j - 1, board, time + 1)
