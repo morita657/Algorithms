aad214481cad7c45b29aeddef44d6b4ed439235e
morita657 <morita657@gmail.com>
2019-09-29 15:03:34 +0900

    add friend circles

---
+++ Leetcode_solutions/Friend Circles.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        visited = [0 for _ in range(len(M))]
+        count = 0
+        for i in range(len(M)):
+            if visited[i] == 0:
+                self.dfs(M, visited, i)
+                count += 1
+        return count
+
+    def dfs(self, M, visited, i):
+        for j in range(len(M)):
+            if M[i][j] == 1 and visited[j] == 0:
+                visited[j] = 1
+                self.dfs(M, visited, j)
