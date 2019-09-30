cac8e678a1161a0fa879d563f1b5696145b46930
morita657 <morita657@gmail.com>
2019-09-30 22:33:49 +0900

    add to lower case

---
+++ Leetcode_solutions/To Lower Case.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        res = ""
+        for char in str:
+            if ord(char) >= 65 and ord(char) <= 90:
+                res += chr(ord(char) + 32)
+            else:
+                res += char
+
+        return res
