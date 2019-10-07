a5031233e21680d5712982fba511e1cf81ec162d
morita657 <morita657@gmail.com>
2019-10-07 22:08:10 +0900

    add k-th symbol in grammar

--- Leetcode_solutions/K-th Symbol in Grammar.py
+++ Leetcode_solutions/K-th Symbol in Grammar.py
@@ -11,3 +11,10 @@ class Solution:
                     row.insert(r + 1, 0)
         print(row)
         return row[K - 1]
+
+
+class Solution(object):
+    def kthGrammar(self, N, K):
+        if N == 1:
+            return 0
+        return (1 - K % 2) ^ self.kthGrammar(N - 1, (K + 1) // 2)
