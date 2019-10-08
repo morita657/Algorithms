a0bbc68c2ce3569467f1dd84389e38fa274a36a9
morita657 <morita657@gmail.com>
2019-10-08 22:02:24 +0900

    add excel sheet column number

--- Leetcode_solutions/Excel Sheet Column Number.py
+++ Leetcode_solutions/Excel Sheet Column Number.py
@@ -1,9 +1,23 @@
-class Solution:
-    def titleToNumber(self, s: str) -> int:
-        ans = []
-        for i in range(len(s)):
-            if i != len(s) - 1:
-                ans.append((ord(s[i]) - 64) * 26**(len(s) - 1 - i))
-            else:
-                ans.append(ord(s[i]) - 64)
-        return sum(ans)
+# class Solution:
+#     def titleToNumber(self, s: str) -> int:
+#         ans = []
+#         for i in range(len(s)):
+#             if i != len(s) - 1:
+#                 ans.append((ord(s[i]) - 64) * 26**(len(s) - 1 - i))
+#             else:
+#                 ans.append(ord(s[i]) - 64)
+#         return sum(ans)
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.val = None
+        self.List = {'val': self.val, 'next': None}
+
+
+m = MyLinkedList()
+print(m)
