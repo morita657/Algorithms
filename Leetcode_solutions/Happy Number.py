fc6d8c7edc2b5206e0017a0592a14d2e51b7fdef
morita657 <morita657@gmail.com>
2019-10-13 20:47:50 +0900

    add happy number

--- Leetcode_solutions/Happy Number.py
+++ Leetcode_solutions/Happy Number.py
@@ -6,3 +6,26 @@ class Solution:
             n = sum([int(x) ** 2 for x in str(n)])
             print(n)
         return n == 1
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        flag = True
+        seen=[]
+        current = []
+        ans=False
+        while True:
+            for i in range(len(str(n))):
+                current.append(int(str(n)[i])**2)
+            total = sum(current)
+            if total==1:
+                ans=True
+                break
+            else:
+                if total in seen:
+                    ans=False
+                    break
+                else:
+                    seen.append(total)
+                    n=total
+                    current=[]
+        return ans
