331be2ed2ffe93edb284bb026ee54acc562c55ce
morita657 <morita657@gmail.com>
2019-10-13 20:48:05 +0900

    add firstuniquecharacterinastring

--- Leetcode_solutions/FirstUniqueCharacterInAString.py
+++ Leetcode_solutions/FirstUniqueCharacterInAString.py
@@ -10,3 +10,14 @@ class Solution:
                 return s.index(key)
         if flag:
             return -1
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d = collections.defaultdict(int)
+        for i in range(len(s)):
+            d[s[i]] += 1
+        for index, key in enumerate(d):
+            if d[key] == 1:
+                return s.index(key)
+        return -1
