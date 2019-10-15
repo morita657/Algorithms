de464355ea1a2042d541b4a6e85866527c71cc5b
morita657 <morita657@gmail.com>
2019-10-15 20:54:42 +0900

    add diagonal traverse

--- Leetcode_solutions/Diagonal Traverse.py
+++ Leetcode_solutions/Diagonal Traverse.py
@@ -1,28 +1,14 @@
 class Solution:
     def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
-        if len(matrix) == 0:
-            return []
-        size = len(matrix) + len(matrix[0])
-        ans = []
-        flag = False
-        for k in range(size):
-            if flag:
-                flag = False
-                i = 0
-                j = k
-                while i <= k and j >= 0:
-                    if i < len(matrix) and j < len(matrix[0]):
-                        ans.append(matrix[i][j])
-                    i += 1
-                    j -= 1
-            else:
-                flag = True
-                i = k
-                j = 0
-                while j <= k and i >= 0:
-                    if i < len(matrix) and j < len(matrix[0]):
-                        ans.append(matrix[i][j])
-                    j += 1
-                    i -= 1
-
-        return ans
+        result = []
+        dd = collections.defaultdict(list)
+        if not matrix:
+            return result
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                dd[i + j + 1].append(matrix[i][j])
+        for k in sorted(dd.keys()):
+            if k % 2 == 1:
+                dd[k].reverse()
+            result += dd[k]
+        return result
