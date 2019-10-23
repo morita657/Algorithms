25912fe27ea5b126e18c1202f22cf4fecd56f69c
morita657 <morita657@gmail.com>
2019-10-23 21:01:15 +0900

    add longest palindromic substring

--- Leetcode_solutions/Longest Palindromic Substring.py
+++ Leetcode_solutions/Longest Palindromic Substring.py
@@ -31,17 +31,17 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if s == None or len(s) < 1:
+        if len(s) == 0:
             return ""
         start = 0
         end = 0
         for i in range(len(s)):
             len1 = self.expandAroundCenter(s, i, i)
             len2 = self.expandAroundCenter(s, i, i + 1)
-            len = max(len1, len2)
-            if len > end - start:
-                start = i - (len - 1) // 2
-                end = i + len // 2
+            length = max(len1, len2)
+            if length > end - start:
+                start = i - (length - 1) // 2
+                end = i + length // 2
         return s[start:end + 1]
 
     def expandAroundCenter(self, s, left, right):
