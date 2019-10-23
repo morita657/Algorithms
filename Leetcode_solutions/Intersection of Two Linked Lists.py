c5c7415d131354515d86f447238965df74699c73
morita657 <morita657@gmail.com>
2019-10-23 21:03:06 +0900

    add intersection of two linked list

--- Leetcode_solutions/Intersection of Two Linked Lists.py
+++ Leetcode_solutions/Intersection of Two Linked Lists.py
@@ -25,3 +25,57 @@ class Solution(object):
             else:
                 b = b.next
         return a
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        hashSet = set()
+
+        while headA:
+            if headA not in hashSet:
+                hashSet.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in hashSet:
+                return headB
+            headB = headB.next
+        return None
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        a=headA
+        b=headB
+        while a != b:
+            # if a == b:
+            #     return True
+            if a == None:
+                a = b
+            else:
+                a = a.next
+            if b == None:
+                b = a
+            else:
+                b = b.next
+        return a
+        
