bd097f7e5310b64a2a4a594342f8c5bbc507171a
morita657 <morita657@gmail.com>
2019-10-24 20:36:29 +0900

    add increasing triplet subsequence

--- Leetcode_solutions/Increasing Triplet Subsequence.py
+++ Leetcode_solutions/Increasing Triplet Subsequence.py
@@ -14,3 +14,17 @@ class Solution(object):
             else:
                 return True
         return False
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        res = []
+        for n in nums:
+            index = bisect.bisect_left(res, n)
+            if index == len(res):
+                res.append(n)
+            else:
+                res[index] = n
+            if len(res) == 3:
+                return True
+        return False
