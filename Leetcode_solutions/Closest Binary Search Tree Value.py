6fd9595bda490626545f9e1eb7eb30aec85889b0
morita657 <morita657@gmail.com>
2019-11-11 23:24:44 +0900

    add Closest Binary Search Tree Value

---
+++ Leetcode_solutions/Closest Binary Search Tree Value.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        def inorder(r: TreeNode):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+
+        return min(inorder(root), key=lambda x: abs(target - x))
