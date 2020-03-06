6649470dc536a987f33c5a435aef44a88bd67920
morita657 <morita657@gmail.com>
2020-03-06 17:55:09 +0900

    note for hashtable solution

--- Leetcode_solutions/containsDuplicate.py
+++ Leetcode_solutions/containsDuplicate.py
@@ -25,7 +25,8 @@ class Solution:
             else:
                 hashTable[index].append(nums[i])
         return False
-        
+# For certain test cases with not very large nn, the runtime of this method can be slower than Approach #2. The reason is hash table has some overhead in maintaining its property. One should keep in mind that real world performance can be different from what the Big-O notation says. The Big-O notation only tells us that for sufficiently large input, one will be faster than the other. Therefore, when nn is not sufficiently large, an O(n)O(n) algorithm can be slower than an O(n \log n)O(nlogn) algorithm.
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         sorted_array = nums.sort()
