49f53383d20697d70513a988bf4d31136f8d7611
morita657 <morita657@gmail.com>
2020-03-06 13:34:43 +0900

    findMin

--- Leetcode_solutions/Find Minimum in Rotated Sorted Array.py
+++ Leetcode_solutions/Find Minimum in Rotated Sorted Array.py
@@ -49,3 +49,22 @@ class Solution(object):
             # if nums[0] is greater than the mid value then this means the smallest value is somewhere to the left
             else:
                 right = mid - 1
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        if len(nums)==1:
+            return nums[0]
+        if nums[0] < nums[right]:
+            return nums[0]
+        while left<right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            if nums[left] < nums[mid]:
+                left = mid+1
+            else:
+                right =mid+1
