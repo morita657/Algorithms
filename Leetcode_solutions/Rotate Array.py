40749e4dca96694734c1de0a1a03eeb8c723f0cd
morita657 <morita657@gmail.com>
2020-03-06 17:49:24 +0900

    reverse array

--- Leetcode_solutions/Rotate Array.py
+++ Leetcode_solutions/Rotate Array.py
@@ -7,3 +7,19 @@ class Solution:
             now = nums.pop()
             nums.insert(0, now)
         return nums
+
+    def rotate1(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def reverse(nums, start, end):
+            while start < end:
+                temp = nums[start]
+                nums[start] = nums[end]
+                nums[end] = temp
+                start += 1
+                end -= 1
+        k %= len(nums)
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
