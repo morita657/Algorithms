9f9c7252b9a2c46bd9a60554d6943c15fd57470d
morita657 <morita657@gmail.com>
2020-03-06 13:34:29 +0900

    inorderTraversal

--- Leetcode_solutions/Binary Tree Inorder Traversal.py
+++ Leetcode_solutions/Binary Tree Inorder Traversal.py
@@ -18,3 +18,39 @@ class Solution(object):
             res.append(root.val)
             res = res + self.inorderTraversal(root.right)
         return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+        while curr is not None or len(stack) > 0:
+            while curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
+    
+    def inorderTraversal1(self, root: TreeNode) -> List[int]:
+        if root == None:
+            return []
+        output = []
+        self.helper(root, output)
+        return output
+    
+    def helper(self, node, result):
+        if node.left:
+            self.helper(node.left, result)
+        result.append(node.val)
+        if node.right:
+            self.helper(node.right, result)
+        
