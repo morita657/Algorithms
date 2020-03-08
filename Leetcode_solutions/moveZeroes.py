44cf037bd4854f85ec565196f7c71125eb910f22
morita657 <morita657@gmail.com>
2020-03-08 13:03:36 +0900

    optimal solution

--- Leetcode_solutions/moveZeroes.py
+++ Leetcode_solutions/moveZeroes.py
@@ -25,3 +25,13 @@ class Solution:
             nums[j] = 0
             
 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lastNonZeroFoundAt = 0
+        for cur in range(len(nums)):
+            if nums[cur] != 0:
+                nums[lastNonZeroFoundAt], nums[cur] = nums[cur], nums[lastNonZeroFoundAt]
+                lastNonZeroFoundAt += 1
