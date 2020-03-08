4212eb3ef235ec02cba1439ddeb653237b2199cd
morita657 <morita657@gmail.com>
2020-03-08 11:53:32 +0900

    reverse solution

--- Leetcode_solutions/plusone.py
+++ Leetcode_solutions/plusone.py
@@ -1,13 +1,20 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        if len(digits) == 0:
+            digits.append(1)
+            return digits
+        carry = 0
         digits[-1] += 1
-        for i in range(len(digits) - 1, 0, -1):
-            if digits[i] != 10:
-                break
-            digits[i] = 0
-            digits[i - 1] += 1
-
-        if digits[0] == 10:
-            digits[0] = 0
-            return [1] + digits
+        if digits[-1] == 10:
+            carry = 1
+            digits[-1] = 0
+        for i in range(len(digits)-2, -1, -1):
+            digits[i] += carry
+            if digits[i] == 10:
+                carry = 1
+                digits[i] = 0
+            else:
+                carry = 0
+        if carry == 1:
+            digits.insert(0, carry)
         return digits
