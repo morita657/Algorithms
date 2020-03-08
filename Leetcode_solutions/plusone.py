2cc366e104c7506efeb949209904c78ffaa20ebf
morita657 <morita657@gmail.com>
2020-03-08 12:18:55 +0900

    another solution

--- Leetcode_solutions/plusone.py
+++ Leetcode_solutions/plusone.py
@@ -18,3 +18,24 @@ class Solution:
         if carry == 1:
             digits.insert(0, carry)
         return digits
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        
+        # move along the input array starting from the end
+        for i in range(n):
+            idx = n - 1 - i
+            print(idx)
+            # set all the nines at the end of array to zeros
+            if digits[idx] == 9:
+                digits[idx] = 0
+            # here we have the rightmost not-nine
+            else:
+                # increase this rightmost not-nine by 1 
+                digits[idx] += 1
+                # and the job is done
+                return digits
+                
+        # we're here because all the digits are nines
+        return [1] + digits
