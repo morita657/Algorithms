2434ba5f5b86b769e9e211c4db95c832a99e5043
morita657 <morita657@gmail.com>
2020-03-10 12:46:11 +0900

    another rotation

--- Leetcode_solutions/rotate_image.py
+++ Leetcode_solutions/rotate_image.py
@@ -6,20 +6,19 @@ class Solution:
         matrix.reverse()
         for i in range(len(matrix) - 1):
             for j in range(i, len(matrix)):
-                print(i, j)
                 [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         return matrix
 
-
-s = Solution()
-print(s.rotate([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]))
-print(s.rotate([
-    [5, 1, 9, 11],
-    [2, 4, 8, 10],
-    [13, 3, 6, 7],
-    [15, 14, 12, 16]
-]))
+    def rotate1(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])        
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                print(matrix[n - 1 - j][i], matrix[n - 1 - i][n - j - 1], matrix[j][n - 1 - i], matrix[i][j])
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
