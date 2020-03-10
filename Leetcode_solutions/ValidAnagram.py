f1f215e18916da68752b6e5eef118594bef156fc
morita657 <morita657@gmail.com>
2020-03-10 15:56:37 +0900

    hashmap O(N) solution

--- Leetcode_solutions/ValidAnagram.py
+++ Leetcode_solutions/ValidAnagram.py
@@ -10,3 +10,18 @@ class Solution:
             else:
                 return False
         return len(cand) == 0
+
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        h = collections.defaultdict(int)
+        for i in range(len(s)):
+            h[s[i]] += 1
+        for j in range(len(t)):
+            if not h[t[j]] or h[t[j]]<=0:
+                return False
+            else:
+                h[t[j]] -= 1
+        return True
