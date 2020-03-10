d65dd17e7015c4d706625502b76f784d87f244dd
morita657 <morita657@gmail.com>
2020-03-10 15:02:31 +0900

    two pointers

--- Leetcode_solutions/reverseInteger.py
+++ Leetcode_solutions/reverseInteger.py
@@ -11,3 +11,23 @@ class Solution:
             return 0
         else:
             return int(ans)
+
+
+    def reverse1(self, x: int) -> int:
+        sign = 1
+        target = str(x)
+        target = list(target)
+        if x < 0:
+            sign = -1
+            target = target[1:]
+        left = 0
+        right = len(target) - 1
+        while left < right:
+            target[left], target[right] = target[right], target[left]
+            left += 1
+            right -= 1
+        target = "".join(target)
+        target = int(target) * sign
+        if target > 2147483647+1 or target < -2147483648-1:
+            return 0
+        return target
