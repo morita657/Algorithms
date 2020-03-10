b41b5fbca728d2b1f375e396c89dedb135b8f039
morita657 <morita657@gmail.com>
2020-03-10 14:10:41 +0900

    another two pointers

--- Leetcode_solutions/reverseString.py
+++ Leetcode_solutions/reverseString.py
@@ -6,3 +6,25 @@ class Solution:
         for i in range(len(s) // 2):
             s[i], s[-i - 1] = s[-i - 1], s[i]
         return s
+
+
+    def reverseString1(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i],s[len(s)-i-1]=s[len(s)-i-1],s[i]
+        return s
+
+
+    def reverseString2(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -=1
+        return s
