339550d23175be63b5982053ea74ec79f5df8bd5
morita657 <morita657@gmail.com>
2020-03-13 10:04:04 +0900

    another solutions

--- Leetcode_solutions/Longest_Common_Prefix.py
+++ Leetcode_solutions/Longest_Common_Prefix.py
@@ -62,3 +62,72 @@ class Solution:
                     return prefix
             prefix += candidate
         return prefix
+
+# Vertical scanning
+# Time complexity: O(S) where S is the sum of all characters
+# Space complexity: O(1)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs == None or len(strs) == 0:
+            return ""
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != c:
+                    return strs[0][0: i]
+        return strs[0]
+
+# Divide and Couquer
+# Time complexity: O(S) where S is the number of all characters in the array, S = m*n
+# Space complexity: O(m*log n)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs == None or len(strs) == 0:
+            return ""
+        return self._longestCommonPrefix(strs, 0, len(strs) - 1)
+        
+    def _longestCommonPrefix(self, strs, l, r):
+        if l == r:
+            return strs[l]
+        else:
+            mid = (l + r) // 2
+            lcpLeft = self._longestCommonPrefix(strs, l, mid)
+            lcpRight = self._longestCommonPrefix(strs, mid + 1, r)
+            return self.commonPrefix(lcpLeft, lcpRight)
+    
+    def commonPrefix(self, left, right):
+        mini = min(len(left), len(right))
+        for i in range(mini):
+            if left[i] != right[i]:
+                return left[0:i]
+        return left[0:mini]
+
+# Binary Search
+# Time complexity: O(S * log N) where S is the sum of all characters in all strings
+# Space complexity: O(1)
+
+import sys
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs == None or len(strs) == 0:
+            return ""
+        minLen = sys.maxsize
+        for string in range(len(strs)):
+            minLen = min(minLen, len(strs[string]))
+        low = 0
+        high = minLen
+        while low <= high:
+            middle = (low + high) // 2
+            if self.isCommonPrefix(strs, middle):
+                low = middle + 1
+            else:
+                high = middle - 1
+        print(low, high)
+        return strs[0][0: (low + high) // 2]
+    
+    def isCommonPrefix(self, strs, length):
+        str1 = strs[0][0:length]
+        for i in range(1, len(strs)):
+            if not strs[i].startswith(str1):
+                return False
+        return True
