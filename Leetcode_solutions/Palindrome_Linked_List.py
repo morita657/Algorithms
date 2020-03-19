42c83c954874e1e9ffa4c8139e018f38b3d1908c
morita657 <morita657@gmail.com>
2020-03-19 09:04:58 +0900

    recursive

--- Leetcode_solutions/Palindrome_Linked_List.py
+++ Leetcode_solutions/Palindrome_Linked_List.py
@@ -20,12 +20,8 @@ class Solution:
             return cand[:mid][::-1] == cand[mid:]
 
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+# Time Complexity: O(N)
+# Space Complexity: O(N)
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
         if head==None:
@@ -35,3 +31,15 @@ class Solution:
             seen.append(head.val)
             head = head.next
         return seen == seen[::-1]
+
+    def isPalindrome1(self, head: ListNode) -> bool:
+        self.front_pointer = head
+        def recursively_check(current_node=head):
+            if current_node is not None:
+                if not recursively_check(current_node.next):
+                    return False
+                if self.front_pointer.val != current_node.val:
+                    return False
+                self.front_pointer = self.front_pointer.next
+            return True
+        return recursively_check()
