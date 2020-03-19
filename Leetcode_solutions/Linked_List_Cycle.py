8ce2415ded5ce447a845429e6ab28493a74a6c2b
morita657 <morita657@gmail.com>
2020-03-19 20:12:48 +0900

    two pointers

--- Leetcode_solutions/Linked_List_Cycle.py
+++ Leetcode_solutions/Linked_List_Cycle.py
@@ -18,3 +18,15 @@ class Solution:
             head = head.next
             rabbit = rabbit.next.next
         return False
+    
+    def hasCycle1(self, head: ListNode) -> bool:
+        if head == None or head.next == None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast == None or fast.next == None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
