448970db6c64572b0a216073b293f34131b4689b
morita657 <morita657@gmail.com>
2020-04-13 20:33:20 +0900

    solution with hash set

--- Leetcode_solutions/Counting Elements.py
+++ Leetcode_solutions/Counting Elements.py
@@ -6,3 +6,12 @@ class Solution:
             if arr[i]+1 in arr:
                 count += 1
         return count
+
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        hash_set = set(arr)
+        count = 0
+        for i in range(len(arr)):
+            if arr[i]+1 in hash_set:
+                count += 1
+        return count
