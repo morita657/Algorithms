852bdde2c8571a147ccd81540d8a25bfd43a02ce
morita657 <morita657@gmail.com>
2020-04-15 17:57:11 +0900

    string comparison

--- Leetcode_solutions/Backspace String Compare.py
+++ Leetcode_solutions/Backspace String Compare.py
@@ -10,3 +10,23 @@ class Solution:
             elif ans:
                 ans.pop()
         return "".join(ans)
+
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        first = ''
+        second = ''
+                
+        def reader(strg):
+            res=""
+            for j in range(len(strg)):
+                if strg[j] == "#":
+                    res = res[:-1]
+                else:
+                    res += strg[j]
+            return res
+        
+        first = reader(S)
+        second = reader(T)
+            
+        return first == second
+            
