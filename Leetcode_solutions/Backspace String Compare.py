46ae1de7756e8bea74b2387a0112a01ed0661771
morita657 <morita657@gmail.com>
2020-04-15 18:06:58 +0900

    two pointers

--- Leetcode_solutions/Backspace String Compare.py
+++ Leetcode_solutions/Backspace String Compare.py
@@ -30,3 +30,17 @@ class Solution:
             
         return first == second
             
+from itertools import zip_longest
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+
+        return all(x == y for x, y in itertools.zip_longest(F(S), F(T)))
