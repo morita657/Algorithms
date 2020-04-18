a6ce63d55ced576ea62b68a5ab772890ae6257d2
morita657 <morita657@gmail.com>
2020-04-18 21:16:59 +0900

    improved approach

--- Leetcode_solutions/Product of Array Except Self.py
+++ Leetcode_solutions/Product of Array Except Self.py
@@ -70,3 +70,16 @@ class Solution:
             tempProd = numpy.prod(temp)
             output.append(tempProd)
         return output
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        answer = [0] * length
+        answer[0] = 1
+        for i in range(1,length):
+            answer[i] = nums[i - 1] * answer[i - 1]
+        r = 1
+        for i in reversed(range(length)):
+            answer[i] = answer[i] * r
+            r *= nums[i]
+        return answer
