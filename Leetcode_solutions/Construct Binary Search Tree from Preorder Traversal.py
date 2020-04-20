e1af896bf2d7cae0ffe817b1122f8a06bfc812ec
morita657 <morita657@gmail.com>
2020-04-20 20:40:26 +0900

    iteration approach

---
+++ Leetcode_solutions/Construct Binary Search Tree from Preorder Traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        n=len(preorder)
+        if not n:
+            return None
+        root = TreeNode(preorder[0])
+        stack = [root, ]
+        for i in range(1, n):
+            node, child = stack[-1], TreeNode(preorder[i])
+            while stack and stack[-1].val < child.val:
+                node = stack.pop()
+                
+            if node.val < child.val:
+                node.right = child
+            else:
+                node.left = child
+            stack.append(child)
+        return root
