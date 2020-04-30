aea96f917a81edb9cb6e460dad30ad36716810fe
morita657 <morita657@gmail.com>
2020-05-01 08:55:36 +0900

    brute force and optimized

--- Leetcode_solutions/Container With Most Water.py
+++ Leetcode_solutions/Container With Most Water.py
@@ -13,3 +13,27 @@ class Solution:
             else:
                 right -= 1
         return area
+# brute force
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        ans = 0
+        
+        for i in range(len(height)):
+            for j in range(i+1, len(height)):
+                total = min(height[i], height[j]) * (j - i)
+                ans = max(total, ans)
+        return ans
+                
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+        l = 0
+        r = len(height) - 1
+        while l < r:
+            maxarea = max(maxarea, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxarea
