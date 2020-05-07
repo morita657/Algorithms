5123ec29f9a3598451dac5799f1f98633010e342
morita657 <morita657@gmail.com>
2020-05-07 14:30:44 +0900

    two pass

--- Leetcode_solutions/Majority Element.py
+++ Leetcode_solutions/Majority Element.py
@@ -19,3 +19,19 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = collections.Counter(nums)
         return count.most_common(1)[0][0]
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        pairs = dict()
+        mck=-1
+        mcv=-1
+        for i in range(len(nums)):
+            if nums[i] in pairs.keys():
+                pairs[nums[i]] +=1
+            else:
+                pairs[nums[i]] =1
+        for key, val in pairs.items():
+            if val>mcv:
+                mcv = val
+                mck = key
+        return mck
