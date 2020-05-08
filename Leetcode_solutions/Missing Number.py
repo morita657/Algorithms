751c5e4486d178f320ac37227ff90a132babe269
morita657 <morita657@gmail.com>
2020-05-08 13:59:33 +0900

    sorting approach

---
+++ Leetcode_solutions/Missing Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        if len(nums)==0:
+            return 0
+        nums.sort()
+        index = 0
+        for i in range(len(nums)):
+            if index != nums[i]:
+                return index
+            index += 1
+        return index
+        
