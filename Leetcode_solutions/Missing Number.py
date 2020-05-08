3459e5f58219f4644f47123bde8fedace077e544
morita657 <morita657@gmail.com>
2020-05-08 13:59:45 +0900

    hashset approach

--- Leetcode_solutions/Missing Number.py
+++ Leetcode_solutions/Missing Number.py
@@ -10,3 +10,11 @@ class Solution:
             index += 1
         return index
         
+
+class Solution:
+    def missingNumber(self, nums):
+        num_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
