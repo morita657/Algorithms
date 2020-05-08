101097e242550b101bd0e256791fa2ec021eb2ef
morita657 <morita657@gmail.com>
2020-05-08 13:11:33 +0900

    Optimized solution

--- Leetcode_solutions/K Closest Points to Origin.py
+++ Leetcode_solutions/K Closest Points to Origin.py
@@ -1,12 +1,5 @@
 class Solution:
-    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        d=[]
-        total=[]
-        for i in range(len(points)):
-            distance = pow(points[i][0],2) + pow(points[i][1],2)
-            d.append([distance, points[i]])
-        d.sort()
-        for j in range(K):
-            total.append(d[j][1])
-        return total
-            
+    def kClosest(self, points, K):
+        points.sort(key=lambda P: P[0] ** 2 + P[1] ** 2)
+        print(points)
+        return points[:K]
